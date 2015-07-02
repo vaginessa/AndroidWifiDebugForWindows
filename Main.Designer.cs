8320e2ff5ba32859ca29485391cc39906272bf1f
JuwanZhu <JuwanZhu@163.com>
2015-07-02 17:15:48 +0800

    Optimizing application and update some file

--- Main.Designer.cs
+++ Main.Designer.cs
@@ -30,6 +30,8 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
             this.mComBoBoxIpList = new System.Windows.Forms.ComboBox();
+            this.mLabelUsage = new System.Windows.Forms.Label();
+            this.mTextBoxOutput = new System.Windows.Forms.TextBox();
             this.mButtonConnect = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
@@ -37,18 +39,49 @@
             // 
             this.mComBoBoxIpList.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
             this.mComBoBoxIpList.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.mComBoBoxIpList.Cursor = System.Windows.Forms.Cursors.Default;
+            this.mComBoBoxIpList.FormatString = "N3";
             this.mComBoBoxIpList.FormattingEnabled = true;
-            this.mComBoBoxIpList.Location = new System.Drawing.Point(13, 12);
+            this.mComBoBoxIpList.Location = new System.Drawing.Point(4, 12);
             this.mComBoBoxIpList.Name = "mComBoBoxIpList";
-            this.mComBoBoxIpList.Size = new System.Drawing.Size(263, 21);
+            this.mComBoBoxIpList.Size = new System.Drawing.Size(273, 21);
             this.mComBoBoxIpList.TabIndex = 0;
+            this.mComBoBoxIpList.SelectedIndexChanged += new System.EventHandler(this.mComBoBoxIpList_SelectedIndexChanged);
+            this.mComBoBoxIpList.KeyDown += new System.Windows.Forms.KeyEventHandler(this.mComBoBoxIpList_KeyDown);
+            // 
+            // mLabelUsage
+            // 
+            this.mLabelUsage.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.mLabelUsage.AutoSize = true;
+            this.mLabelUsage.Location = new System.Drawing.Point(6, 65);
+            this.mLabelUsage.Name = "mLabelUsage";
+            this.mLabelUsage.Size = new System.Drawing.Size(271, 65);
+            this.mLabelUsage.TabIndex = 2;
+            this.mLabelUsage.Text = "使用须知：\n1、确定电脑已接入局域网。\n2、确定手机和电脑能相互ping通。\n3、确定手机上安装有【wifi调试】(可在应用市场搜\n索wifi debug)并确定" +
+    "其已打开。";
+            // 
+            // mTextBoxOutput
+            // 
+            this.mTextBoxOutput.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.mTextBoxOutput.ForeColor = System.Drawing.SystemColors.Window;
+            this.mTextBoxOutput.Location = new System.Drawing.Point(4, 133);
+            this.mTextBoxOutput.Multiline = true;
+            this.mTextBoxOutput.Name = "mTextBoxOutput";
+            this.mTextBoxOutput.ReadOnly = true;
+            this.mTextBoxOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.mTextBoxOutput.Size = new System.Drawing.Size(273, 280);
+            this.mTextBoxOutput.TabIndex = 4;
             // 
             // mButtonConnect
             // 
-            this.mButtonConnect.Location = new System.Drawing.Point(201, 39);
+            this.mButtonConnect.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.mButtonConnect.AutoSize = true;
+            this.mButtonConnect.ImageAlign = System.Drawing.ContentAlignment.TopRight;
+            this.mButtonConnect.Location = new System.Drawing.Point(202, 39);
             this.mButtonConnect.Name = "mButtonConnect";
             this.mButtonConnect.Size = new System.Drawing.Size(75, 23);
-            this.mButtonConnect.TabIndex = 1;
+            this.mButtonConnect.TabIndex = 2;
             this.mButtonConnect.Text = "Connect";
             this.mButtonConnect.UseVisualStyleBackColor = true;
             this.mButtonConnect.Click += new System.EventHandler(this.buttonConnectClick);
@@ -59,7 +92,9 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
             this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(288, 163);
+            this.ClientSize = new System.Drawing.Size(281, 420);
+            this.Controls.Add(this.mTextBoxOutput);
+            this.Controls.Add(this.mLabelUsage);
             this.Controls.Add(this.mButtonConnect);
             this.Controls.Add(this.mComBoBoxIpList);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -68,12 +103,15 @@
             this.Text = "Android Wifi Debug";
             this.Load += new System.EventHandler(this.mainLoad);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.ComboBox mComBoBoxIpList;
+        private System.Windows.Forms.Label mLabelUsage;
+        private System.Windows.Forms.TextBox mTextBoxOutput;
         private System.Windows.Forms.Button mButtonConnect;
     }
 }
--- Main.cs
+++ Main.cs
@@ -7,7 +7,7 @@ using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace AndroidWifiDebug
@@ -19,16 +19,24 @@ namespace AndroidWifiDebug
             InitializeComponent();
         }
 
+        private String mConfigFilePath;
+        private String mConfigFileName;
         private void mainLoad(object sender, EventArgs e)
         {
             // set config file's path
-            //mConfigFilePath = System.Environment.CurrentDirectory;    // current directory
+            //path = System.Environment.CurrentDirectory;    // current directory
             mConfigFilePath = Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments) + @"\AndroidWifiDebug\Config\"; // my documents in user's home directory
             mConfigFileName = "his.ini";
 
-            if (File.Exists(mConfigFilePath + mConfigFileName))  // if exists his.ini file, load item from his.ini file
+            loadIpHistory(mConfigFilePath, mConfigFileName);
+
+        }
+
+        private void loadIpHistory(String path, String name)
+        {
+            if (File.Exists(path + name))  // if exists his.ini file, load item from his.ini file
             {
-                StreamReader sr = new StreamReader(mConfigFilePath + mConfigFileName, Encoding.Default);
+                StreamReader sr = new StreamReader(path + name, Encoding.Default);
 
                 while (sr.Peek() > 0)
                 {
@@ -41,14 +49,15 @@ namespace AndroidWifiDebug
             }
             else
             {
-                Directory.CreateDirectory(mConfigFilePath);
+                Directory.CreateDirectory(path);
             }
         }
 
-        String mConfigFilePath;
-        String mConfigFileName;
+
         private void buttonConnectClick(object sender, EventArgs e)
         {
+            mButtonConnect.UseWaitCursor = true;
+            // If input string is new, remember it.
             if (!mComBoBoxIpList.Items.Contains(mComBoBoxIpList.Text))
             {
                 mComBoBoxIpList.Items.Insert(0, mComBoBoxIpList.Text);
@@ -58,16 +67,41 @@ namespace AndroidWifiDebug
             StreamWriter sw = new StreamWriter(mConfigFilePath + mConfigFileName);
             for (int i = 0; i < mComBoBoxIpList.Items.Count; i++)
             {
-                sw.WriteLine(mComBoBoxIpList.Items[i]);
+                String line = mComBoBoxIpList.Items[i].ToString();
+                if (line != @"")  // delete empty line
+                {
+                    sw.WriteLine(line);
+                }
             }
 
             sw.Close();
 
-            Cmd_Adb_connect(mComBoBoxIpList.Text);
+            // prevent main form is not respondint, do it in background.
+            //Thread connectThread = new Thread(Cmd_Adb_connect);
+            //connectThread.Start(mComBoBoxIpList.Text);
+
+            using (BackgroundWorker bw = new BackgroundWorker())
+            {
+                bw.RunWorkerCompleted += new RunWorkerCompletedEventHandler(bwRunWorkerCompleted);
+                bw.DoWork += new DoWorkEventHandler(Cmd_Adb_connect);
+                bw.RunWorkerAsync(mComBoBoxIpList.Text);
+            }
+
         }
 
-        private void Cmd_Adb_connect(String ip)
+        // after input finished, enter "Enter" Key will same as "click button click".
+        private void mComBoBoxIpList_KeyDown(object sender, KeyEventArgs e)
         {
+            if (e.KeyCode == Keys.Enter)
+            {
+                buttonConnectClick(sender, e);
+            }
+        }
+
+        static StringBuilder output = new StringBuilder();
+        static void Cmd_Adb_connect(object sender, DoWorkEventArgs args)
+        {
+            String ip = args.Argument.ToString();
             Process process = new Process();
             process.StartInfo.FileName = "cmd.exe";
             process.StartInfo.UseShellExecute = false;
@@ -78,10 +112,20 @@ namespace AndroidWifiDebug
 
             process.StandardInput.WriteLine("adb connect " + ip);
             process.StandardInput.WriteLine("exit");
-            String output = process.StandardOutput.ReadToEnd();
-            MessageBox.Show(output);
+
+            output.Append(process.StandardOutput.ReadToEnd());
+
         }
 
+        void bwRunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            this.mTextBoxOutput.Text = output.ToString();
+            mButtonConnect.UseWaitCursor = false;
+        }
 
+        private void mComBoBoxIpList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
