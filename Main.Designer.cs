204226acb7927ce91eeea0b1b88ad7acdeff3738
JuwanZhu <JuwanZhu@163.com>
2015-07-02 11:40:31 +0800

    connect to phone and remember input history

---
+++ Main.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace AndroidWifiDebug
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mConBoBoxIpList = new System.Windows.Forms.ComboBox();
+            this.mButtonConnect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // mConBoBoxIpList
+            // 
+            this.mConBoBoxIpList.FormattingEnabled = true;
+            this.mConBoBoxIpList.Location = new System.Drawing.Point(13, 12);
+            this.mConBoBoxIpList.Name = "mConBoBoxIpList";
+            this.mConBoBoxIpList.Size = new System.Drawing.Size(263, 21);
+            this.mConBoBoxIpList.TabIndex = 0;
+            // 
+            // mButtonConnect
+            // 
+            this.mButtonConnect.Location = new System.Drawing.Point(201, 39);
+            this.mButtonConnect.Name = "mButtonConnect";
+            this.mButtonConnect.Size = new System.Drawing.Size(75, 23);
+            this.mButtonConnect.TabIndex = 1;
+            this.mButtonConnect.Text = "Connect";
+            this.mButtonConnect.UseVisualStyleBackColor = true;
+            this.mButtonConnect.Click += new System.EventHandler(this.buttonConnectClick);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(288, 163);
+            this.Controls.Add(this.mButtonConnect);
+            this.Controls.Add(this.mConBoBoxIpList);
+            this.Name = "Main";
+            this.Text = "Android Wifi Debug";
+            this.Load += new System.EventHandler(this.mainLoad);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox mConBoBoxIpList;
+        private System.Windows.Forms.Button mButtonConnect;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AndroidWifiDebug
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void mainLoad(object sender, EventArgs e)
+        {
+            mConfigFilePath = System.Environment.CurrentDirectory + @"\data.ini";
+
+            if (File.Exists(mConfigFilePath))
+            {
+                StreamReader sr = new StreamReader(mConfigFilePath, Encoding.Default);
+
+                while (sr.Peek() > 0)
+                {
+                    mConBoBoxIpList.Items.Add(sr.ReadLine());
+                }
+                sr.Close();
+
+                mConBoBoxIpList.Text = (String)mConBoBoxIpList.Items[0];
+            }
+        }
+
+        String mConfigFilePath;
+        private void buttonConnectClick(object sender, EventArgs e)
+        {
+            if (!mConBoBoxIpList.Items.Contains(mConBoBoxIpList.Text))
+            {
+                mConBoBoxIpList.Items.Insert(0, mConBoBoxIpList.Text);
+            }
+            
+
+            // save content of combobox to file
+            StreamWriter sw = new StreamWriter(mConfigFilePath);
+            for (int i = 0; i < mConBoBoxIpList.Items.Count; i++)
+            {
+                sw.WriteLine(mConBoBoxIpList.Items[i]);
+            }
+
+            sw.Close();
+
+            Cmd_Adb_connect(mConBoBoxIpList.Text);
+        }
+
+        private void Cmd_Adb_connect(String ip)
+        {
+            Process process = new Process();
+            process.StartInfo.FileName = "cmd.exe";
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardInput = true;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.CreateNoWindow = true;
+            process.Start();
+
+            process.StandardInput.WriteLine("adb connect " + ip);
+            process.StandardInput.WriteLine("exit");
+            String output = process.StandardOutput.ReadToEnd();
+            MessageBox.Show(output);
+        }
+
+        
+    }
+}
